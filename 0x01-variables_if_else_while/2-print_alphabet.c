4e98d9759a716254fa110617b2cd597da482bb80
cynthianekesa <c.nekesa@alustudent.com>
2023-02-22 02:15:20 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main -prints the alphabet in lowercase by using putchar only
+ * printf and puts are forbidden
+ * Return: 0
+*/
+int main(void)
+{
+	char lc;
+
+	for (lc = 'a'; lc <= 'z'; lc++)
+	{
+		putchar(lc);
+	}
+	putchar("\n");
+	return (0);
+}
