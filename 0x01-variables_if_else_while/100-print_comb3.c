f6464905b40834c970336a2de7d34cd60b9b202a
cynthianekesa <c.nekesa@alustudent.com>
2023-02-22 08:38:33 -0800

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main-prints all possible different combinations of two digits
+ * using putchar only
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int c, y;
+
+	for (c = '0'; c < '9'; c++)
+	{
+	for (y = 0 + 1; y <= '9'; y++)
+	{
+	if (c != y)
+	{
+	putchar(c);
+	putchar(y);
+	if (c == '8' && y == '9')
+	continue;
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	}
+	putchar('\n');
+	return (0);
+}
